1a6e76d493c3a2d1e88f60c672477fc9113395a8
Yusuf Incekara <yincekara@gmail.com>
2022-06-07 10:32:07 +0300

    First Commit

--- Vs2005SampleIntegration/Form1.cs
+++ Vs2005SampleIntegration/Form1.cs
@@ -16,5 +16,15 @@ namespace Vs2005SampleIntegration
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Deneme");
+        }
     }
 }
